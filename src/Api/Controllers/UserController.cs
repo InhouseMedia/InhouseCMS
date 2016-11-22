2b65635d6bbd7d13d7d702c3f06506314c6623a1
Ramon Klanke <rklanke@binck.nl>
2016-11-22 17:14:36 +0100

    Visual Studio setup

---
+++ src/Api/Controllers/UserController.cs
@@ -0,0 +1,149 @@
+﻿namespace Api.Controllers
+{
+    using System;
+	using System.Collections.Generic;
+	using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Mvc;
+	
+    using Api.Models;
+	using Api.Repositories;
+
+    [Route("[controller]")]
+    public class UserController : Controller
+    {
+	    private readonly IUserRepository _repository;
+
+        public UserController(IUserRepository settings)
+        {
+            _repository = settings;
+        }
+   
+        [HttpGet]
+        //[Authorize]
+        public async Task<IEnumerable<User>> Get()
+        {
+            return await _repository.AllUsers(); 
+        }
+   
+        [HttpPost]
+        //[AllowAnonymous]
+        //[ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(string username, string password, bool rememberMe, string returnUrl = "") {
+
+			Console.Write("Login ");
+            if (ModelState.IsValid)
+            {
+                var user = await _repository.Login(username, password) as User;
+				
+				if(user == null || !user.Active || user.LockedDate >= DateTime.UtcNow){
+					return new StatusCodeResult(204); // 204 No Content
+				}
+				/*
+				var hasher = PasswordHasher();
+                var result = hasher.VerifyHashedPassword(user, user.Password, password);
+				
+				//var result = new PasswordH.VerifyHashedPassword(user.PasswordHash, password);
+  				if (result == PasswordVerificationResult.Success){
+					  var identity = new ClaimsIdentity("Password");
+						identity.AddClaim(new Claim("sub", user.UserName));
+						identity.AddClaim(new Claim("role", "user"));
+						//identity.AddClaim(new Claim("email", user.Email));
+						//context.Validated(identity);
+                    //await SignInAsync(user, rememberMe);
+                    //return RedirectToLocal(returnUrl);
+					var temp = new ObjectResult(identity);
+					Console.Write("Identity " + username);
+					return temp;
+				  }*/
+				
+				/*
+				
+                if (user != null)
+                {
+					var identity = new ClaimsIdentity("Password");
+						identity.AddClaim(new Claim("sub", user.UserName));
+						identity.AddClaim(new Claim("role", "user"));
+						//identity.AddClaim(new Claim("email", user.Email));
+						//context.Validated(identity);
+                    //await SignInAsync(user, rememberMe);
+                    //return RedirectToLocal(returnUrl);
+					var temp = new ObjectResult(identity);
+					Console.Write("Identity " + username);
+					return temp;
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Invalid username or password.");
+                }*/
+            }
+			return NotFound();
+            // If we got this far, something failed, redisplay form
+            // return View(model);
+           // return RedirectToAction("Index", "Login");
+        }
+
+        private ActionResult RedirectToLocal(string returnUrl)
+        {
+	        if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+	        return RedirectToAction("Index", "Home");
+        }
+
+/*
+        private IAuthenticationManager AuthenticationManager
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().Authentication;
+            }
+        }
+
+        private async Task SignInAsync(ApplicationUser user, bool isPersistent)
+        {
+            AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
+            
+            ///Open Question- Hear it create claimIdentity. But we nothing add as such Claims but just User object.
+            //public virtual Task<ClaimsIdentity> CreateIdentityAsync(TUser user, string authenticationType);
+
+            var identity = await UserManager1.CreateIdentityAsync(user, DefaultAuthenticationTypes.ApplicationCookie);
+            //var identity = await UserManager1.CreateAsync(user);//, DefaultAuthenticationTypes.ApplicationCookie);
+
+            AuthenticationManager.SignIn(new AuthenticationProperties() { IsPersistent = isPersistent }, identity);
+        }*/
+        /*
+                [HttpPost]
+                public IActionResult CreateSpeaker([FromBody] Speaker speaker)
+                {            
+                    if (!ModelState.IsValid)
+                    {
+                        //Context.Response.StatusCode = 400;
+                       return new StatusCodeResult(400);
+                    }
+
+                    _speakerRepository.Add(speaker);
+
+                    //string url = Url.RouteUrl("GetByIdRoute", new { id = speaker.Id.ToString() }, Request.Scheme, Request.Host.ToUriComponent());
+                   // Context.Response.StatusCode = 201;
+                    //Context.Response.Headers["Location"] = url;
+
+                    //return CreatedAtRoute("GetByIdRoute", new { id = speaker.Id.ToString() }, Request.Scheme, Request.Host.ToUriComponent());       
+                    return CreatedAtRoute("GetByIdRoute", new { id = speaker.Id.ToString() },Request.Host.ToUriComponent());
+                }
+
+                [HttpDelete("{id:length(24)}")]
+                public IActionResult DeleteSpeaker(string id)
+                {
+                    if (_speakerRepository.Remove(new ObjectId(id)))
+                    {
+                        return new StatusCodeResult(204); // 204 No Content
+                    }
+                    else
+                    {
+                        return HttpNotFound();
+                    }
+                }*/
+    }
+}
---
+++ src/Api/Models/UserModel.cs
@@ -0,0 +1,41 @@
+namespace Api.Models
+{
+	using System;
+	using MongoDB.Bson;
+	using MongoDB.Bson.Serialization.Attributes;
+
+	public class User
+    {
+        public ObjectId Id { get; set; }
+        [BsonElement("UserName")]
+        public string UserName { get; set; }    
+        [BsonElement("Password")]
+        public string Password { get; set; }
+        [BsonElement("FirstName")]
+        public string FirstName { get; set; }
+        [BsonElement("LastName")]
+        public string LastName { get; set; }
+        [BsonElement("Prefix")]
+        public string Prefix { get; set; }
+        [BsonElement("Email")]
+        public string Email { get; set; }
+        [BsonElement("EmailConfirmed")]
+        public bool EmailConfirmed { get; set; } 
+        [BsonElement("PhoneNumber")]
+        public string PhoneNumber { get; set; }
+        [BsonElement("PhoneNumberConfirmed")]
+        public bool PhoneNumberConfirmed { get; set; }
+        [BsonElement("SessionId")]
+        public string SessionId { get; set; }
+        [BsonElement("Active")]
+        public bool Active { get; set; } 
+        [BsonElement("LockedDate")]
+        public DateTime LockedDate { get; set; }
+        [BsonElement("LastLoginDate")]
+        public DateTime LastLoginDate { get; set; }
+        [BsonElement("CreatedDate")]
+        public DateTime CreatedDate { get; set; }
+        [BsonElement("ChangedDate")]
+        public DateTime ChangedDate { get; set; }
+    }
+}
