64f9a02c5bbca11aa27b0118d143f0afcc312696
Ramon Klanke <info@inhousemedia.nl>
2016-12-14 17:27:16 +0100

    First implementation of gulp
    - still need to create minification within gulp

--- src/Web/gulpfile.js
+++ src/Web/gulpfile.js
@@ -5,7 +5,10 @@ var gulp = require("gulp"),
     rimraf = require("rimraf"),
     concat = require("gulp-concat"),
     cssmin = require("gulp-cssmin"),
-    uglify = require("gulp-uglify");
+    uglify = require("gulp-uglify"),
+    less = require("gulp-less"),
+    path = require("path"),
+    rename = require("gulp-rename");
 
 var webroot = "./wwwroot/";
 
@@ -18,28 +21,46 @@ var paths = {
     concatCssDest: webroot + "css/site.min.css"
 };
 
-gulp.task("clean:js", function (cb) {
+gulp.task("clean:js", function(cb) {
     rimraf(paths.concatJsDest, cb);
 });
 
-gulp.task("clean:css", function (cb) {
+gulp.task("clean:css", function(cb) {
     rimraf(paths.concatCssDest, cb);
 });
 
 gulp.task("clean", ["clean:js", "clean:css"]);
 
-gulp.task("min:js", function () {
+gulp.task("min:js", function() {
     return gulp.src([paths.js, "!" + paths.minJs], { base: "." })
         .pipe(concat(paths.concatJsDest))
         .pipe(uglify())
         .pipe(gulp.dest("."));
 });
 
-gulp.task("min:css", function () {
-    return gulp.src([paths.css, "!" + paths.minCss])
+gulp.task("min:css", function() {
+    return gulp.src(["./Content/**/css/*.css", "!" + paths.minCss])
         .pipe(concat(paths.concatCssDest))
         .pipe(cssmin())
         .pipe(gulp.dest("."));
 });
 
-gulp.task("min", ["min:js", "min:css"]);
+gulp.task('less', function() {
+    return gulp.src('./Content/**/site.bootstrap.less')
+        .pipe(less({
+            paths: [path.join(__dirname, 'less', 'includes')]
+        }))
+        .pipe(rename(function(path) {
+            var filename = path.dirname.replace('/less', '');
+            path.dirname = path.dirname.replace('/less', '/css');
+            //path.dirname = webroot + '/css';
+            path.basename = filename + '.bootstrap';
+            path.extname = '.css';
+        }))
+        .pipe(gulp.dest(function(file) {
+            //var filename = file.base.replace('/less', '/css');
+            return file.base;
+        }));
+});
+
+gulp.task("min", ["less", "min:js", "min:css"]);
