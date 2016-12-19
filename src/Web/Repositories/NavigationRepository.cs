c6905427a3cfcd2b5a88b655d08aef331dc789eb
Unknown <rklanke@binck.nl>
2016-12-19 21:50:09 +0100

    api connection for navigation component view

--- src/Web/Repositories/NavigationRepository.cs
+++ src/Web/Repositories/NavigationRepository.cs
@@ -7,10 +7,12 @@ namespace Web.Repositories
 	using Library.Models;
     using Web.Connections;
     using System.Globalization;
+    using System.Threading.Tasks;
 
     public interface INavigationRepository
 	{
 		NavigationSitemap GetNavigationItem(string path);
+		Task<IEnumerable<NavigationSitemap>> GetNavigation();
 	}
 
 	public class NavigationRepository : ConnectionRepository, INavigationRepository
@@ -28,6 +30,14 @@ namespace Web.Repositories
 		public NavigationSitemap GetNavigationItem(string path)
 		{
 			return _navigation.FirstOrDefault(n => n.Url.ToLower().Equals(path.ToLower()));
+		}
+
+		public async Task<IEnumerable<NavigationSitemap>> GetNavigation()
+		{
+			var locale = CultureInfo.DefaultThreadCurrentUICulture.Name;
+			var conn = await _api.ConnectAsync(locale + "/navigation/sitemap");
+			var result = conn.Content.ReadAsAsync<IEnumerable<NavigationSitemap>>();
+			return await result;
 		}
 	}
 }
