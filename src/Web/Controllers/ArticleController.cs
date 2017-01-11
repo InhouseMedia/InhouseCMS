a7cf59220671a3118ad76d9eeeb7fb799fd25190
Ramon Klanke <rklanke@binck.nl>
2017-01-11 11:39:52 +0100

    Add extra config

--- src/Web/Controllers/ArticleController.cs
+++ src/Web/Controllers/ArticleController.cs
@@ -1,41 +1,48 @@
 namespace Web.Controllers
 {
 	using Microsoft.AspNetCore.Mvc;
-    using Microsoft.Extensions.Localization;
+	using Microsoft.Extensions.Localization;
+
+	using MailKit.Net.Smtp;
+	using MimeKit;
 
 	using Newtonsoft.Json;
 
+	using System;
 	using System.Text.RegularExpressions;
 	using System.Threading.Tasks;
 
+	using Library.Config;
 	using Library.Models;
 	using Web.Repositories;
-	
+
 	//Done in startup
 	//[ServiceFilter(typeof(LocalizationActionFilter))]
 	public class ArticleController : Controller
-    {
-        private readonly IStringLocalizer<ArticleController> _localizer;
-        private readonly IArticleRepository _repository;
+	{
+		private readonly SiteConfig _config;
+		private readonly IStringLocalizer<ArticleController> _localizer;
+		private readonly IArticleRepository _repository;
 
-        public ArticleController(IArticleRepository repository, IStringLocalizer<ArticleController> localizer)
-        {
-            _localizer = localizer;
-            _repository = repository;
-        }
+		public ArticleController(IArticleRepository repository, IStringLocalizer<ArticleController> localizer, ConfigRepository config)
+		{
+			_localizer = localizer;
+			_repository = repository;
+			_config = config.GetConfig();
+		}
 
-        public async Task<IActionResult> Index(string id)
-        {
-            if (!ModelState.IsValid)
+		public async Task<IActionResult> Index(string id)
+		{
+			if (!ModelState.IsValid)
 				return new StatusCodeResult(500); // 500 Internal Server Error
 
-            var result = await _repository.GetPage(id);
+			var result = await _repository.GetPage(id);
 
-            if (result == null)
+			if (result == null)
 				return new StatusCodeResult(204); // 204 No Content
 
-            return View(result);
-        }
+			return View(result);
+		}
 
 		[HttpPost]
 		[ValidateAntiForgeryToken]
@@ -43,46 +50,46 @@ namespace Web.Controllers
 		{
 			if (!ModelState.IsValid) return View();
 
-			var body = "";
+			var body = new BodyBuilder();
 			var articleId = Request.Form["ArticleId"];
 			var contentId = Request.Form["ContentId"];
 
 			var response = await _repository.GetContent(articleId, contentId);
+			
 			// Error
 			if (response == null)
 				return new StatusCodeResult(204); // 204 No Content
 
 			var formOptions = JsonConvert.DeserializeObject<FormModel>(response.Code);
-			
+
 			foreach (var field in formOptions.Fields)
 			{
 				var name = new Regex("[\\s_-]").Replace(field.Label, "");
-				body += "<p><b>" + field.Label + ":</b> " + Request.Form[name];
+				body.HtmlBody += "<p><b>" + field.Label + ":</b> " + Request.Form[name];
 			}
-			/*
-			var message = new MailMessage();
-			message.From = new MailAddress(MvcApplication.Config.Company.Contact.Email);
-			message.To.Add(new MailAddress(formOptions.Mail.To)); //Should be from code (json object)
-			message.Body = body;
+			
+			var message = new MimeMessage();
+			message.From.Add(new MailboxAddress(_config.Company.Contact.Email));
+			message.To.Add(new MailboxAddress(formOptions.Mail.To)); //Should be from code (json object)
 			message.Subject = formOptions.Mail.Subject;
-			message.IsBodyHtml = true;
+			message.Body = body.ToMessageBody();
 
-			try
+			using (var client = new SmtpClient())
 			{
-				SmtpClient client = new SmtpClient();
-				client.Port = 25;
-				client.Host = "smtp.gmail.com"; //or SMTP name
-				client.Timeout = 10000;
-				client.DeliveryMethod = SmtpDeliveryMethod.Network;
-				client.UseDefaultCredentials = false;
-				client.Credentials = new NetworkCredential("ramonklanke@gmail.com", "Evelienklein@1");
-				client.Send(message);
+				try
+				{
+					if (!_config.Mailserver.Oauth2) client.AuthenticationMechanisms.Remove("XOAUTH2");
+
+					client.Connect(_config.Mailserver.Smtp, _config.Mailserver.Port, _config.Mailserver.UseSsl);
+					client.Authenticate(_config.Mailserver.Account, _config.Mailserver.Password);
+					client.Send(message);
+					client.Disconnect(true);
+				}
+				catch (Exception ex)
+				{
+					return new StatusCodeResult(450);
+				}
 			}
-			catch (Exception ex)
-			{
-				throw new InvalidOperationException("No SMTP Mail sent", ex);
-				//MessageBox.Show(ex.Message);
-			}*/
 
 			return View();
 		}
