926960d34b5f4cf7f929eb4fbb86e43e31c007fb
Unknown <rklanke@binck.nl>
2017-02-21 22:21:44 +0100

    Treeview helper for action ComponentView
    And Active buttons for General ComponentView

--- src/Cms/ViewComponents/ActionsViewComponents.cs
+++ src/Cms/ViewComponents/ActionsViewComponents.cs
@@ -1,16 +1,38 @@
 namespace Cms.ViewComponents
 {
+	using Library.Config;
 	using Library.Models;
+	using Library.Repositories;
 
 	using Microsoft.AspNetCore.Mvc;
 
+	using System.Collections.Generic;
 	using System.Threading.Tasks;
 
 	public class ActionsViewComponent : ViewComponent
 	{
+		private readonly SiteConfig _config;
+
+		public ActionsViewComponent(ConfigRepository config)
+		{
+			_config = config.GetConfig();
+		}
+
 		public async Task<IViewComponentResult> InvokeAsync(ArticlePage model)
 		{
-			return View(model);
+			var actions = _config.Controllers.Article.Actions;
+
+			var items = new List<NavigationSitemap>();
+
+			foreach(var item in actions){
+				var nav = new NavigationSitemap();
+				nav.Title = item;
+
+				items.Add(nav);
+			}
+
+
+			return View(items);
 		}
 	}
 }
--- src/Cms/ViewComponents/GeneralViewComponents.cs
+++ src/Cms/ViewComponents/GeneralViewComponents.cs
@@ -1,23 +1,29 @@
 namespace Cms.ViewComponents
 {
+	using Cms.Controllers;
+
 	using Library.Config;
 	using Library.Models;
 	using Library.Repositories;
 
 	using Microsoft.AspNetCore.Mvc;
+	using Microsoft.Extensions.Localization;
 
 	using System.Collections.Generic;
-	using System.Threading.Tasks;
 	using System.Reflection;
+	using System.Text.RegularExpressions;
+	using System.Threading.Tasks;
 
 	public class GeneralViewComponent : ViewComponent
 	{
 
 		private readonly SiteConfig _config;
+		private readonly IStringLocalizer<ResourceController> _sharedLocalizer;
 
-		public GeneralViewComponent(ConfigRepository config)
+		public GeneralViewComponent(ConfigRepository config, IStringLocalizer<ResourceController> sharedLocalizer)
 		{
 			_config = config.GetConfig();
+			_sharedLocalizer = sharedLocalizer;
 		}
 
 		public async Task<IViewComponentResult> InvokeAsync(ArticlePage model)
@@ -37,13 +43,14 @@ namespace Cms.ViewComponents
 				var x = (int)item.GetValue(se.Visibility);
 				// TODO: Active should be changed in the database from boolean to int
 				var active = model.Active ? 1 : 0;
+				var name = Regex.Match(item.Name, @"\<(\w+)\>").Groups[1].Value;
 
 				var boxObject = new BoxVisibility
 				{
 					Active = (x == active) ? "active" : "",
-					Name = item.Name,
+					Name = name,
 					Key = x,
-					Translation = "" //manager.GetString(item.Name)
+					Translation = _sharedLocalizer["{0}", name]
 				};
 
 				ViewBag.Visibility.Add(boxObject);
