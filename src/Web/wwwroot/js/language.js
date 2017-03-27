b2ad4cc11944e904ad87c6737a641f8a98fdc6fc
Ramon Klanke <info@inhousemedia.nl>
2017-03-27 17:00:35 +0200

    fix click on language toggle

---
+++ src/Web/wwwroot/js/language.js
@@ -0,0 +1,28 @@
+﻿var language = (function () {
+	'use strict';
+
+	var _element = $('.box-locale form input[type=radio]');
+
+	var _init = function () {
+		_setEvent();
+	};
+
+	var _setEvent = function () {
+		_element.change(_submitLanguage);
+	};
+
+	var _submitLanguage = function (e) {
+		e.preventDefault();
+		e.target.form.submit();
+	};
+
+	return {
+		init: function () {
+			_init();
+		}
+	}
+})();
+
+$(document).ready(function () {
+	language.init();
+});
